9c7eb160da3a064b2da32d1710abbd875cdd2492
yaswanthc1782 <128622700+yaswanthc1782@users.noreply.github.com>
2023-07-27 19:33:09 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from calculate import *
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route('/')
+@app.route("/home")
+def home():
+    return render_template("index.html")
+@app.route('/result',methods=['POST','GET'])
+def calc():
+    convection=request.form["convection"]
+    velocity=request.form["velocity"]
+    typeht=request.form["typeht"]
+    shape=request.form["shape"]
+    length=request.form["length"]
+    breadth=request.form["breadth"]
+    thickness=request.form["thickness"]
+    surftemp=request.form["surftemp"]
+    surrtemp=request.form["surrtemp"]
+    name=display(convection,velocity,typeht,shape,length,breadth,thickness,surftemp,surrtemp)
+    return render_template("index.html",name=name)
+if __name__=="__main__":
+    app.run()
